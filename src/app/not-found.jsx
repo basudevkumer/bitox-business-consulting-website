51e54ae4b017495e8bbb24366046d34e088d5238
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-23 22:21:21 +0600

    remove not found design

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,62 +1,9 @@
-// import React from "react";
-// import Link from "next/link"; // ← এটা add করো
-
-// const NotFound = () => {
-//   return (
-//     <div className="min-h-screen bg-[#f0ebe1] flex flex-col items-center justify-center font-sans">
-
-//       {/* 404 Big Text */}
-//       <div className="relative flex items-start justify-center select-none mb-8">
-//         <span className="absolute -top-4 -left-6 text-[#f97316] text-5xl font-black leading-none">
-//           ✳
-//         </span>
-//         <h1
-//           className="text-[180px] md:text-[220px] font-black leading-none tracking-tighter"
-//           style={{
-//             background: "linear-gradient(180deg, #1a1a1a 40%, #c2410c 100%)",
-//             WebkitBackgroundClip: "text",
-//             WebkitTextFillColor: "transparent",
-//             backgroundClip: "text",
-//           }}
-//         >
-//           404.
-//         </h1>
-//       </div>
-
-//       {/* Orange Banner */}
-//       <div className="w-[1920px] bg-secondary py-10.75  flex items-center justify-center mb-10">
-//         <p className="font-semibold text-[72px] leading-20.5 uppercase text-[rgb(255,255,255)]">
-//           Sorry, we can&apos;t find the page you&apos;re looking for
-//         </p>
-//       </div>
-
-//       {/* Description */}
-//       <p className="text-gray-500 text-sm md:text-base text-center max-w-md px-4 mb-8 leading-relaxed">
-//         It looks like you&apos;ve taken a wrong turn, but don&apos;t worry we&apos;re here
-//         <br />
-//         to help! Head back to the homepage please
-//       </p>
-
-//       {/* Button */}
-//       <Link
-//         href="/"
-//         className="flex items-center gap-2 border border-gray-400 rounded-full px-6 py-2.5 text-sm text-gray-700 hover:bg-gray-200 transition-colors duration-200"
-//       >
-//         Back to home page
-//         <span className="text-xs">▶</span>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default NotFound;
-
 import React from 'react'
 
-export default function NotFound() {
+export default function not-found() {
   return (
     <div>
-      
+      <h2>Not found</h2>
     </div>
   )
 }

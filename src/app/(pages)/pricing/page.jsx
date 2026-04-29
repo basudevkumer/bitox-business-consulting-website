26abc8275a3c0dad85319fd6ca28a798ec1c787e
basudevkumar <jhulonkumar40@gmail.com>
2026-04-30 00:50:57 +0600

    feat: added PricingPlan  resuable component in  Pricing page

--- src/app/(pages)/pricing/page.jsx
+++ src/app/(pages)/pricing/page.jsx
@@ -1,12 +1,12 @@
 import InnerPageHero from "@/components/ui/InnerPageHero";
+import PricingPlan from "@/components/homeTwo/PricingPlan";
 import React from "react";
 
 const Pricing = () => {
   return (
     <>
-      <InnerPageHero
-        title="Bitox for Complete Solutions"
-      />
+      <InnerPageHero title="Bitox for Complete Solutions" />
+      <PricingPlan />
     </>
   );
 };

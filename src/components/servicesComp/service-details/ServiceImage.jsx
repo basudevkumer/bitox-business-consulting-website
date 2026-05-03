4aea360a1ba9090f1fb9db49ffc33081e6e4f401
basudevkumar <jhulonkumar40@gmail.com>
2026-05-03 11:50:54 +0600

    feat: in project added responsive  in ServiceImage component

--- src/components/servicesComp/service-details/ServiceImage.jsx
+++ src/components/servicesComp/service-details/ServiceImage.jsx
@@ -5,20 +5,37 @@ import Image from "next/image";
 const ServiceImage = () => {
   const { servivesDetailsBannaer } = allImages;
   return (
-    <section className="py-[120px]">
+    <section className="py-[60px] sm:py-[80px] lg:py-[120px]">
       <Container>
-        <div className="w-full h-[690px] relative z-10">
+        <div className="w-full h-[220px] sm:h-[380px] md:h-[500px] lg:h-[690px] relative z-10 group overflow-hidden rounded-[6px]">
           <Image
             alt="servivesDetailsBannaer"
-            className="object-cover rounded-[6px] z-0"
+            className="object-cover rounded-[6px] z-0 transition-transform duration-700 ease-in-out group-hover:scale-105"
             fill
             src={servivesDetailsBannaer}
           />
-          <div className="w-[470px] space-y-[10px] h-[242px]   flex flex-col justify-center items-center bottom-[35px] right-[30px]  absolute rounded-t-full bg-secondary">
-            <h2 className="font-bold text-[90px]/[90px] text-bg-secondaryTwo ">
-              29+{" "}
+
+          {/* Dark overlay on hover */}
+          <div className="absolute inset-0 bg-black/0 group-hover:bg-black/30 transition-all duration-500 ease-in-out rounded-[6px] z-10" />
+
+          {/* Stats card */}
+          <div className="
+            w-[170px] h-[120px]
+            sm:w-[260px] sm:h-[160px]
+            md:w-[360px] md:h-[200px]
+            lg:w-[470px] lg:h-[242px]
+            flex flex-col justify-center items-center
+            space-y-[6px] sm:space-y-[10px]
+            right-[10px] sm:right-[20px] lg:right-[30px]
+            bottom-[-120px] sm:bottom-[-160px] md:bottom-[-200px] lg:bottom-[-242px]
+            group-hover:bottom-[15px] sm:group-hover:bottom-[25px] lg:group-hover:bottom-[35px]
+            absolute rounded-t-full bg-secondary
+            transition-all duration-600 ease-in-out z-20
+          ">
+            <h2 className="font-bold text-[40px]/[40px] sm:text-[60px]/[60px] md:text-[75px]/[75px] lg:text-[90px]/[90px] text-bg-secondaryTwo">
+              29+
             </h2>
-            <h5 className="  text-bg-secondaryTwo   headingSix font-medium">
+            <h5 className="text-bg-secondaryTwo headingSix font-medium text-center px-2">
               Our talented consultant
             </h5>
           </div>

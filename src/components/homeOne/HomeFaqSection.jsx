86ebeaf05660ed6c814dfa731846c4c70e15ff2d
basudevkumar <jhulonkumar40@gmail.com>
2026-05-10 01:44:46 +0600

    feat: added HomeFaqSection section in home pages

---
+++ src/components/homeOne/HomeFaqSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from '../ui/Container'
+import FaqBody from '../ui/FaqBody'
+
+const HomeFaqSection = () => {
+  return (
+    <section className='py-[120px]'>
+        <Container size='lg'>
+            <div>
+                <h2 className='max-w-[981px] headingTwo text-primary font-bold text-center mx-auto mb-[80px]'>Frequently Asked Questions: Clear, 
+Concise Answers</h2>
+                <FaqBody/>
+            </div>
+        </Container>
+    </section>
+  )
+}
+
+export default HomeFaqSection

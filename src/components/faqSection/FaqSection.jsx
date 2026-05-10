131d2ba52235f68ddec1c988be6e23c5e26dc850
basudevkumar <jhulonkumar40@gmail.com>
2026-05-10 10:17:03 +0600

    feat: in ServiceDelFaq added fully responsive

--- src/components/faqSection/FaqSection.jsx
+++ src/components/faqSection/FaqSection.jsx
@@ -1,19 +1,13 @@
-
 import Container from "../ui/Container";
 import FaqBody from "../ui/FaqBody";
 import FaqHeading from "./FaqHeading";
 
 const FaqSection = () => {
- 
-
   return (
     <section className="py-12 sm:py-16 md:py-20 lg:py-[120px]">
       <Container size="lg">
-        
-        <FaqHeading/>
-        <FaqBody/>
-
-        
+        <FaqHeading />
+        <FaqBody />
       </Container>
     </section>
   );
--- src/components/servicesComp/service-details/ServiceDelFaq.jsx
+++ src/components/servicesComp/service-details/ServiceDelFaq.jsx
@@ -8,15 +8,15 @@ const ServiceDelFaq = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
   return (
-    <section className="mb-[120px]">
+    <section className="mb-12 sm:mb-16 md:mb-20   lg:mb-[120px] ">
       <Container size="lg">
-        <div className="grid grid-cols-3 gap-[125px]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10  md:gap-x-5  md:gap-y-0 lg:gap-x-[125px]">
           <div>
             <h3 className="headingThree max-w-[408px] text-start font-bold text-primary">
               Frequently asked question
             </h3>
           </div>
-          <div className="col-span-2 space-y-[10px]">
+          <div className=" lg:col-span-2 space-y-[10px]">
             {faqData.general.map((items, index) => (
               <AccordionItem
                 key={index}

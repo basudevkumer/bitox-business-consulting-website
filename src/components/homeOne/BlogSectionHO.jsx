17dac01981a4189c105ce43c588597d38b021cca
basudevkumar <jhulonkumar40@gmail.com>
2026-05-13 13:03:01 +0600

    feat: added link  'See all case studies' button to route to blog page

--- src/components/homeOne/BlogSectionHO.jsx
+++ src/components/homeOne/BlogSectionHO.jsx
@@ -28,9 +28,11 @@ const BlogSectionHO = () => {
           </Responsive.Grid>
         </div>
         <div className=" mt-[20px] md:mt-[30px] lg::mt-[60px]">
-          <button className="w-full cursor-pointer text-center py-5  text-primary font-medium text-base border border-[#0000001a] rounded-[6px]">
-            See all case studies
-          </button>
+          <Link href={"/blog"}>
+            <button className="w-full cursor-pointer text-center py-5  text-primary font-medium text-base border border-[#0000001a] rounded-[6px]">
+              See all case studies
+            </button>
+          </Link>
         </div>
       </Container>
     </section>
